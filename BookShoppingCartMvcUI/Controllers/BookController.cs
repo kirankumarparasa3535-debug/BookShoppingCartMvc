8802f06e582efcd1310668a8787fd86609067f52
kirankumarparasa3535-debug <DELL@DESKTOP-4VNENL5>
2025-09-30 14:55:57 +0530

    today changes message

--- BookShoppingCartMvcUI/Controllers/BookController.cs
+++ BookShoppingCartMvcUI/Controllers/BookController.cs
@@ -215,7 +215,8 @@ public class BookController : Controller
         }
         return RedirectToAction(nameof(Index));
         Console.WriteLine("printing");
-        Console.WritrLine("bookname");
+        Console.WritreLine("bookname");
+        Console.WriteLine("message");
     }
 
 }
